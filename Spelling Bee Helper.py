a192c25f95809060c2b759d8da6da0f71f8313db
Rob Niederman <R_Niederman@hotmail.com>
2022-02-22 16:53:06 -0500

    Add files via upload

--- Spelling Bee Helper.py
+++ Spelling Bee Helper.py
@@ -6,7 +6,6 @@ from urllib.request import urlopen
 sb_letters = ['F', 'U', 'N', 'L', 'I', 'B', 'E']
 # Center Letter Should Always be 1st
 word_url = 'https://norvig.com/ngrams/enable1.txt'
-
 # word_url = 'http://www-personal.umich.edu/~jlahcer/wordlist'
 # word_url = 'http://www.mieliestronk.com/corncob_caps.txt'
 
@@ -20,7 +19,7 @@ def unique_letters(word):
 def spelling_bee_points(wrd):
     uniq_lttrs = unique_letters(wrd)
     word_len = len(wrd)
-    pts = 1 if word_len == 4 else word_len
+    pts = word_len if word_len > 4 else 1
     long_adder = (uniq_lttrs == 7) * 7
     pts = pts + long_adder
     return pts
@@ -34,27 +33,26 @@ def build_word_list(url):
     hc = list(map(len, w))
     ul = list(map(unique_letters, w))
     p = list(map(spelling_bee_points, w))
-    dict = {'Word': w, 'Length': hc, 'Unique Letters': ul, 'Score': p}
-    df1 = pd.DataFrame(dict)
-    df2 = df1[(df1['Unique Letters']) <= 7]
-    df2 = df2.sort_values(['Score', 'Word'], ascending=(False, True))
-    return df2
+    d = {'Word': w, 'Length': hc, 'Unique Letters': ul, 'Score': p}
+    df = pd.DataFrame(d)
+    df = df[(df['Unique Letters']) <= 7]
+    df = df.sort_values(['Score', 'Word'], ascending=(False, True))
+    return df
 
 
-def get_words(letters, mwl):
-    center_letter = letters[0]
-    hc = mwl
-    hc = hc[hc['Word'].str.contains(center_letter)]
+def sb_helper(letters, mwl):
+    center = letters[0]
+    hc = mwl[mwl['Word'].str.contains(center)]
     alphabet = set(st.ascii_uppercase)
     invalid_letters = alphabet.difference(set(letters))
     for bad_lttr in invalid_letters:
         hc = hc[~hc['Word'].str.contains(bad_lttr)]
     cols = [0, 3]
-    df = hc[hc.columns[cols]]
-    return df
+    hc = hc[hc.columns[cols]]
+    return hc
 
 
 word_list = build_word_list(word_url)
-sb_soln = get_words(sb_letters, word_list)
+sb_soln = sb_helper(sb_letters, word_list)
 
 print(sb_soln)

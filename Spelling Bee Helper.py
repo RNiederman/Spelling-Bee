4d70ed7e26a68c790dd2e5ac5c5dfee866c5ecf8
Rob Niederman <R_Niederman@hotmail.com>
2022-02-23 16:52:01 -0500

    Add files via upload

--- Spelling Bee Helper.py
+++ Spelling Bee Helper.py
@@ -5,8 +5,10 @@ import sys
 from urllib.request import urlopen
 
 
-sb_letters = 'fbeilnu'
+sb_letters = 'debtjoc'
 # Center Letter Should Always be 1st
+letters_reqd = 7
+
 word_url = 'https://norvig.com/ngrams/enable1.txt'
 # word_url = 'http://www-personal.umich.edu/~jlahcer/wordlist'
 # word_url = 'http://www.mieliestronk.com/corncob_caps.txt'
@@ -19,11 +21,11 @@ def unique_letters(word):
 
 
 def spelling_bee_points(wrd):
-    uniq_lttrs = unique_letters(wrd)
-    word_len = len(wrd)
-    pts = word_len if word_len > 4 else 1
-    long_adder = (uniq_lttrs == 7) * 7
-    pts = pts + long_adder
+    ul = unique_letters(wrd)
+    wl = len(wrd)
+    pts = wl if wl > 4 else 1
+    panagram = (ul == 7) * 7
+    pts = pts + panagram
     return pts
 
 
@@ -32,9 +34,9 @@ def build_word_list(url):
     w = f.read().decode('utf-8').upper().split()
     w = list(filter(lambda word: len(word) >= 4, w))
     w = list(filter(lambda word: "S" not in (word), w))
-    ul = list(map(unique_letters, w))
+    u = list(map(unique_letters, w))
     p = list(map(spelling_bee_points, w))
-    d = {'Word': w, 'Unique Letters': ul, 'Score': p}
+    d = {'Word': w, 'Unique Letters': u, 'Score': p}
     df = pd.DataFrame(d)
     df = df[(df['Unique Letters']) <= 7]
     df = df.sort_values(['Score', 'Word'], ascending=(False, True))
@@ -42,8 +44,8 @@ def build_word_list(url):
 
 
 def sb_helper(letters, mwl):
-    ltrs_0 = letters.upper()
-    ltrs = re.findall("[A-Z]", ltrs_0)
+    ltrs = letters.upper()
+    ltrs = re.findall("[A-Z]", ltrs)
     center = ltrs[0]
     hc = mwl[mwl['Word'].str.contains(center)]
     alphabet = set(st.ascii_uppercase)
@@ -55,8 +57,10 @@ def sb_helper(letters, mwl):
     return hc
 
 
-test = unique_letters(sb_letters) != 7
-if test:
+reg_ex = re.compile('[^A-Za-z]')
+tester = reg_ex.sub('', sb_letters)
+bail_out = unique_letters(tester) != letters_reqd
+if bail_out:
     sys.exit("Wrong Amount of Letters")
 word_list = build_word_list(word_url)
 sb_soln = sb_helper(sb_letters, word_list)

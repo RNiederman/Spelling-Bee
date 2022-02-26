fee4104fc14250cc0e120da5c8e41e1b7cab891b
Rob Niederman <R_Niederman@hotmail.com>
2022-02-26 16:40:33 -0500

    Add files via upload

--- Wordle Helper.py
+++ Wordle Helper.py
@@ -14,7 +14,7 @@ o = re.sub("[^A-Z]", "", o)
 i = include.upper()
 i = re.sub("[^A-Z]", "", i)
 
-a = "".join(answer.split())
+a = ''.join(answer.split())
 a = a[:5].upper()
 a = re.sub("[^A-Z]", "?", a)
 
@@ -33,17 +33,17 @@ if len(i) > 0:
     r = re.compile(patt)
     w = list(filter(r.findall, w))
 
-if a != "?????":
+if a != '?????':
     patt = a.replace("?", "[A-Z]{1}")
     r = re.compile(patt)
     w = list(filter(r.match, w))
 
 
 def scrabble_score(word):
-    d = {"A": 1, "B": 3, "C": 3,  "D": 2, "E": 1, "F": 4, "G": 2,
-         "H": 4, "I": 1, "J": 8,  "K": 5, "L": 1, "M": 3, "N": 1,
-         "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1,
-         "V": 4, "W": 4,  "X": 8, "Y": 4, "Z": 10}
+    d = {'A': 1, 'B': 3, 'C': 3,  'D': 2, 'E': 1, 'F': 4, 'G': 2,
+         'H': 4, 'I': 1, 'J': 8,  'K': 5, 'L': 1, 'M': 3, 'N': 1,
+         'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1,
+         'V': 4, 'W': 4,  'X': 8, 'Y': 4, 'Z': 10}
 
     s = 0
     for letter in word.upper():
